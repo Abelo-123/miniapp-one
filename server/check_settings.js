f00caf6686614f77c377ecfaef7b9416eb9fd766
abel <abeloabate01@gmail.com>
2026-05-08 23:57:51 +0300

    asdasdasopo

--- server/check_settings.js
+++ server/check_settings.js
@@ -1,14 +1,17 @@
 import pool from './config/database.js';
 
-async function checkSettings() {
+async function check() {
     try {
-        const [rows] = await pool.execute('SELECT * FROM settings');
-        console.log('Settings:', rows);
+        const [rows] = await pool.execute('SELECT * FROM service_custom WHERE service_id = 2807');
+        console.log('Service 2807 custom settings:', rows);
+        
+        const [settings] = await pool.execute('SELECT * FROM settings WHERE setting_key = "rate_multiplier"');
+        console.log('Rate multiplier:', settings);
+    } catch (e) {
+        console.error(e);
+    } finally {
         process.exit(0);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
     }
 }
 
-checkSettings();
+check();
